6a269e848755de65e6a6699b12347ce73e8d9a64
Anton Augustsson <anton.augustsson99@gmail.com>
2024-05-20 19:45:20 +0200

    angular http client

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    console.log('ngOnInit')
+    this.http
+      .get('https://www.google.com/')
+      .subscribe(res => {
+        console.log('res', res)
+      })
+  }
 }
